e95f3ecd03a86853e70b61f28b8e23bb41f85d53
Alok Nikhil <anikhil@confluent.io>
2021-04-02 15:32:43 -0700

    Kafka Siege: TCP load testing of Kafka Brokers

---
+++ cmd/kafkasiegecli/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net"
+	"sync/atomic"
+	"time"
+)
+
+var (
+	brokerEP string
+	conns    uint
+)
+
+func init() {
+	flag.StringVar(&brokerEP, "broker-endpoint", "", "Broker Endpoint (including port)")
+	flag.UintVar(&conns, "connections", 1, "No. of connections to launch a siege with")
+}
+
+func main() {
+	completed := uint64(0)
+
+	for i := uint(0); i < conns; i++ {
+		go func() {
+			conn, err := net.Dial("tcp", brokerEP)
+			if err != nil {
+				log.Printf("TCP dial error: %s\n", err.Error())
+				return
+			}
+			if err = conn.Close(); err != nil {
+				log.Printf("Connection close error: %s\n", err.Error())
+			}
+			atomic.AddUint64(&completed, 1)
+		}()
+	}
+
+	for atomic.LoadUint64(&completed) < uint64(conns) {
+		log.Printf("Waiting on %d threads to complete\n", uint64(conns)-atomic.LoadUint64(&completed))
+		time.Sleep(100)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aloknnikhil/kafkasiege
+
+go 1.15
